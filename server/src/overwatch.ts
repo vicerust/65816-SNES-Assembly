0fa0d4d5b85c09e9888914adae3e1073eb244042
Anthony Heddings <anthony@etherion.io>
2019-07-08 20:47:48 -0400

    Added hover for RAM Addresses

--- server/src/overwatch.ts
+++ server/src/overwatch.ts
@@ -1,12 +1,13 @@
 import { CompletionItem, MarkupContent, MarkupKind, TextDocument, Position, SymbolInformation, SymbolKind, CompletionItemKind, TextDocumentPositionParams, SignatureHelp, ParameterInformation } from 'vscode-languageserver'
 import * as asm from "./asm.json"
-
+import { RAM } from "./RAM.js"
 
 interface looseObj {
 	[key: string]: any
 }
 
-var functionsRegExp = new RegExp(/defaultslot|slotsize|slot|id|name|slowrom|lorom|hirom|cartridgetype|romsize|sramsize|country|licenseecode|version|cop|brk|abort|nmi|irq|reset|irqbrk|semifree|header_off|force|DEFAULTSLOT|SLOTSIZE|SLOT|ID|NAME|SLOWROM|LOROM|HIROM|CARTRIDGETYPE|ROMSIZE|SRAMSIZE|COUNTRY|LICENSEECODE|VERSION|ABORT|NMI|IRQ|RESET|IRQBRK|SEMIFREE|HEADER_OFF|FORCE|ADC|AND|ASL|BCC|BCS|BEQ|BIT|BMI|BNE|BPL|BRA|BRK|BRL|BVC|BVS|CLC|CLD|CLI|CLV|CMP|COP|CPX|CPY|DEC|DEX|DEY|EOR|INC|INX|INY|JML|JMP|JSL|JSR|LDA|LDX|LDY|LSR|MVN|MVP|NOP|ORA|PEA|PEI|PER|PHA|PHB|PHD|PHK|PHP|PHX|PHY|PLA|PLB|PLD|PLP|PLX|PLY|REP|ROL|ROR|RTI|RTL|RTS|SBC|SEC|SED|SEI|SEP|STA|STP|STX|STY|STZ|TAX|TAY|TCD|TCS|TDC|TRB|TSB|TSC|TSX|TXA|TXS|TXY|TYA|TYX|WAI|WDM|XBA|XCE/)
+var functionsRegExp = new RegExp(/\$[\dABCDEF]+|defaultslot|slotsize|slot|id|name|slowrom|lorom|hirom|cartridgetype|romsize|sramsize|country|licenseecode|version|cop|brk|abort|nmi|irq|reset|irqbrk|semifree|header_off|force|DEFAULTSLOT|SLOTSIZE|SLOT|ID|NAME|SLOWROM|LOROM|HIROM|CARTRIDGETYPE|ROMSIZE|SRAMSIZE|COUNTRY|LICENSEECODE|VERSION|ABORT|NMI|IRQ|RESET|IRQBRK|SEMIFREE|HEADER_OFF|FORCE|ADC|AND|ASL|BCC|BCS|BEQ|BIT|BMI|BNE|BPL|BRA|BRK|BRL|BVC|BVS|CLC|CLD|CLI|CLV|CMP|COP|CPX|CPY|DEC|DEX|DEY|EOR|INC|INX|INY|JML|JMP|JSL|JSR|LDA|LDX|LDY|LSR|MVN|MVP|NOP|ORA|PEA|PEI|PER|PHA|PHB|PHD|PHK|PHP|PHX|PHY|PLA|PLB|PLD|PLP|PLX|PLY|REP|ROL|ROR|RTI|RTL|RTS|SBC|SEC|SED|SEI|SEP|STA|STP|STX|STY|STZ|TAX|TAY|TCD|TCS|TDC|TRB|TSB|TSC|TSX|TXA|TXS|TXY|TYA|TYX|WAI|WDM|XBA|XCE/g)
+var ramRegExp = new RegExp(/\$[\dABCDEF]/)
 
 var Opcodes = asm.opcodes.map((d) => {
 	return {
@@ -53,16 +54,39 @@ export function hoverHandler(pos: Position, doc: TextDocument): string | MarkupC
 	if(matches[0] == null) return "";
 	var match = matches[0].match;
 	
-	var r = Opcodes.find((d)=>d.label == match)
-	if(!r) return "";
+	if (match.match(ramRegExp) != null) {
+		if (match.length == 5) { match = "$7E" + match.substr(1,4); }
+		var ramMatch = RAM.find((r) => r.address == match)
+		if(ramMatch) {
+			let markdown: MarkupContent = {
+				kind: MarkupKind.Markdown,
+				value: [
+					"Address: " + ramMatch.address,
+					'\n---',
+					"Length: " + ramMatch.length,
+					'\n---',
+					"Type: " + ramMatch.type,
+					'\n---',
+					ramMatch.descriptio,
+				].join('\n')
+			};
 
-	if(r.detail) {
-		let markdown: MarkupContent = {
-			kind: MarkupKind.Markdown,
-			value: r.detail
-		};
 		return markdown
-	}
+		} else {
+			return "Could Not Parse RAM Address.";
+		}
+	} else {
+		var r = Opcodes.find((d)=>d.label == match)
+		if(!r) return "";
 
-	return r.detail as string
+		if(r.detail) {
+			let markdown: MarkupContent = {
+				kind: MarkupKind.Markdown,
+				value: r.detail
+			};
+			return markdown
+		}
+
+		return r.detail as string
+	}
 }
